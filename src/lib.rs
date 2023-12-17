aee2a68af5af40f8a17167f0119ef3a8e7516e65
taka231 <taka23131@gmail.com>
2023-12-17 16:18:50 +0900

    type check用にtypeinferを使うことにした

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod alpha;
 pub mod anf;
 pub mod ast;
 pub mod parser;
+pub mod typeinfer;
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use SimplyTypedLambdaCalculusCompiler::{
     alpha::AlphaConvEnv,
     anf::{ANFConverter, ANFs},
     parser::expr_parser,
+    typeinfer::TypeInfer,
 };
 
 fn main() {
     let ast = expr_parser::expr(r#"(\f. \x. f x) ((\x. \y. x + y) 2) 3"#).unwrap();
     let alpha_conv_env = AlphaConvEnv::new();
     let ast = alpha_conv_env.alpha_conversion(ast).unwrap();
+    let mut typeinfer = TypeInfer::new(alpha_conv_env.id());
+    let ty = typeinfer.type_infer(&ast).unwrap();
+    println!("{:?}", ty.simplify());
     let mut anfconverter = ANFConverter::new(alpha_conv_env.id());
     let mut anfs = ANFs {
         anfs: Vec::new(),
